550c0410a26449ee1e44c9a8d8354756f74232a1
liu253928127 <253928127@qq.com>
2018-11-04 09:54:23 +0800

    修改ResultDTO, 添加lombok注解

--- ly-common/src/main/java/com/leyou/common/dto/ResultDTO.java
+++ ly-common/src/main/java/com/leyou/common/dto/ResultDTO.java
@@ -1,10 +1,15 @@
 package com.leyou.common.dto;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 /**
  * @author coofive
  *
  */
-public class ResultDTO {
+@Data
+@Accessors(chain = true)
+public class ResultDTO<T> {
     /**
      * 是否成功
      */
@@ -23,53 +28,6 @@ public class ResultDTO {
     /**
      * 返回数据
      */
-    private Object data;
+    private T data;
 
-    public ResultDTO(boolean flag, Integer code, String message, Object data) {
-        this.flag = flag;
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-
-    public ResultDTO(boolean flag, Integer code, String message) {
-        this.flag = flag;
-        this.code = code;
-        this.message = message;
-    }
-
-    public ResultDTO() {
-    }
-
-    public boolean isFlag() {
-        return flag;
-    }
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
